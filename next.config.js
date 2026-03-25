2ffb75f5a08ce9f89664ca54559c031301eada6f
adamweberabstrakt <adam.weber@abstraktmg.com>
2026-03-24 23:37:38 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ['pdfkit'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'abstrakt-ai-brand-lift.vercel.app',
+      },
+    ],
   },
 };
 
